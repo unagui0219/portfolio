4546473b529bdd209edd645f8811cf22b3396617
unagibuchi <striker0219@gmail.com>
2022-05-01 21:44:16 +0900

    refactor:Card.vue and Lang.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div :class="classes">
     <div class="card-title">
       <h2>{{ title }}</h2>
     </div>
@@ -14,6 +14,19 @@ export default {
   props: {
     title: String,
     content: String,
+    large: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        "card-default": true,
+        "card-large": this.large,
+      };
+    },
   },
 };
 </script>
@@ -21,12 +34,29 @@ export default {
 #nav {
   text-align: center;
 }
-.card {
+.card-default {
   box-shadow: 0 0.5rem 1rem hsl(0 0% 0% / 20%);
   border-radius: 10px;
-  width: 25%;
-  padding: 0.5rem 1rem;
+  width: 80%;
+  padding: 0.5rem 1rem 1rem;
   background-color: #fff;
-  margin-left: 1rem;
+  margin: 0 auto 1rem;
+}
+.card-large {
+  width: 80%;
+}
+.card-row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 2rem auto 0;
+  width: 90%;
+}
+.card-body {
+  margin: 0;
+  padding: 0;
+}
+.card-cover {
+  width: 25rem;
 }
 </style>
--- src/views/Lang.vue
+++ src/views/Lang.vue
@@ -1,34 +1,9 @@
 <template>
   <div class="lang">
     <PageTitle title="Lang" />
-    <div class="lang-body">
-      <div class="lang-row">
-        <Card
-          title="日本語"
-          content="22年使ってますが、この中で一番難しい言語だと思ってます。語彙力をもっと増やして、思考力のベースを固めていきたいです。"
-        />
-        <Card
-          title="HTML/CSS"
-          content="高校生の時から書いてます。CSS設計なども勉強しています。"
-        />
-        <Card
-          title="Ruby"
-          content="書き始めてから今年で4年目になります。プログラミング言語の中で一番書いてます。"
-        />
-      </div>
-      <div class="lang-row">
-        <Card
-          title="JavaScript"
-          content="こちらも4年になりますが、最初はバニラとjQueryばかり書いていました。最近はVue.jsばかり書いています。"
-        />
-        <Card
-          title="Python"
-          content="大学2年生の時にインターンで初めて触れました。主にWeb系でDjangoというフレームワークを使ってます。"
-        />
-        <Card
-          title="Swift"
-          content="去年から少し勉強を始めました。ちょこっとだけかけます。"
-        />
+    <div class="card-row">
+      <div class="card-cover" v-for="(item, index) in langItems" :key="index">
+        <Card :title="item.title" :content="item.content" />
       </div>
     </div>
   </div>
@@ -40,17 +15,44 @@ import Card from "@/components/Card.vue";
 
 export default {
   name: "Lang",
+  data() {
+    return {
+      langItems: langItems,
+    };
+  },
   components: {
     PageTitle,
     Card,
   },
 };
+var langItems = [
+  {
+    title: "日本語",
+    content:
+      "22年使ってますが、この中で一番難しい言語だと思ってます。語彙力をもっと増やして、思考力のベースを固めていきたいです。",
+  },
+  {
+    title: "HTML/CSS",
+    content: "高校生の時から書いてます。CSS設計なども勉強しています。",
+  },
+  {
+    title: "Ruby",
+    content:
+      "書き始めてから今年で4年目になります。プログラミング言語の中で一番書いてます。",
+  },
+  {
+    title: "JavaScript",
+    content:
+      "こちらも4年になりますが、最初はバニラとjQueryばかり書いていました。最近はVue.jsばかり書いています。",
+  },
+  {
+    title: "Python",
+    content:
+      "大学2年生の時にインターンで初めて触れました。主にWeb系でDjangoというフレームワークを使ってます。",
+  },
+  {
+    title: "Swift",
+    content: "去年から少し勉強を始めました。ちょこっとだけかけます。",
+  },
+];
 </script>
-
-<style>
-.lang-row {
-  display: flex;
-  justify-content: center;
-  margin-top: 2rem;
-}
-</style>
